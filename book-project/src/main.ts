d5fe6338780d543527b1a59f4ce6f8a609868ee1
More <262203@NTTDATA.COM>
2022-12-12 08:44:00 +0530

    changed the port number

--- book-project/src/main.ts
+++ book-project/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import {cors} from 'cors'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, ); //{ cors: true }
+  app.enableCors();
+
+  //app.use(cors());
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET, PUT, POST, DELETE',
+//     allowedHeaders: 'Content-Type, Authorization',
+// });
+
+await app.listen(80);
 }
 bootstrap();
